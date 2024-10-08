6f2cf8978be7f95bc08e909d255615d3578a9962
kiruhak <kiruhak1991@gmail.com>
2024-10-09 00:29:11 +0700

    env?

--- app.vue
+++ app.vue
@@ -1,11 +1,26 @@
 <template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
+  </ul>
   <NuxtLayout>
-      <NuxtPage />
+    <NuxtPage />
   </NuxtLayout>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { supabase } from "@/utils/supabase";
+const todos = ref<any>([]);
+
+async function getTodos() {
+  const { data } = await supabase.from("todos").select();
+  todos.value = data;
+}
+
+onMounted(() => {
+  getTodos();
+});
+</script>
 
 <style lang="scss">
-@import 'assets/styles/index';
+@import "assets/styles/index";
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,15 @@ import { defineNuxtConfig } from 'nuxt/config';
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/device', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@nuxt/ui', 'nuxt-swiper', '@nuxtjs/device'],
+  modules: [
+    "@nuxtjs/device",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@nuxt/ui",
+    "nuxt-swiper",
+    "@nuxtjs/device",
+    "@nuxtjs/supabase",
+  ],
   colorMode: {
     preference: 'light',
   },
@@ -11,8 +19,26 @@ export default defineNuxtConfig({
     cookieOptions: {
       sameSite: 'strict',
     },
-    storage: 'localStorage',
+    storage: "localStorage",
   },
+  runtimeConfig: {
+    public: {
+      sb_url: process.env.SUPABASE_URL ?? "",
+      sb_key: process.env.SUPABASE_KEY ?? "",
+    },
+  },
+  supabase: {
+    url: process.env.SUPABASE_URL ?? "",
+    key: process.env.SUPABASE_KEY ?? "",
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+      include: undefined,
+      exclude: [],
+      cookieRedirect: false,
+    },
+  },
+
   vite: {
     css: {
       preprocessorOptions: {
